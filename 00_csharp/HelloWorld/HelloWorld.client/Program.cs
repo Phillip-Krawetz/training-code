b0fb78c71fca56404442a355ea96eeeacf68e88a
phillip krawetz <phillip.kraw@gmail.com>
2019-10-24 15:35:07 -0500

    add HelloWorld

---
+++ 00_csharp/HelloWorld/HelloWorld.client/Program.cs
@@ -0,0 +1,18 @@
+﻿using sc = System.Console; //import with alias
+
+namespace HelloWorld.client
+{
+  /// <summary>
+  /// contains start of the program
+  /// </summary>
+  internal class Program
+  {
+    /// <summary>
+    /// starts the program
+    /// </summary>
+    private static void Main()
+    {
+        sc.WriteLine("Hello World!");
+    }
+  }
+}
